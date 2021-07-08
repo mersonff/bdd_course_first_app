747047ab199f31b66c0ff10a361b2e2db215d4ac
Emerson Freitas <mersonsi@gmail.con>
2021-07-08 16:37:53 -0300

    Mudando o banco de dados para o PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_08_180941) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "routines", force: :cascade do |t|
     t.string "name"
     t.text "description"

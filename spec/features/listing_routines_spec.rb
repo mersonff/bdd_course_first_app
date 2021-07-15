3b5858145e02117d524dc2e4f8f0cc99c1d48152
Emerson Freitas <mersonsi@gmail.con>
2021-07-15 15:30:28 -0300

    Adicionando restrições na listagem de treinos

--- spec/features/listing_routines_spec.rb
+++ spec/features/listing_routines_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Listando Treinos', type: feature do
 
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
+    @john = User.create(email: "john@example.com", password: "password")
 
-    @routine = Routine.create(name: 'Treino A', description: 'Supino 3x10', user: john)
-    @routine2 = Routine.create(name: 'Treino B', description: 'Agachamento 3x10', user: john)
+    @routine = Routine.create(name: 'Treino A', description: 'Supino 3x10', user: @john)
+    @routine2 = Routine.create(name: 'Treino B', description: 'Agachamento 3x10', user: @john)
   end
 
-  scenario 'Um usuário lista todos os treinos' do
+  scenario 'Um usuário lista todos os treinos e o usuário não esta logado' do
     visit '/'
     expect(page).to have_content(@routine.name)
     expect(page).to have_content(@routine.description)
@@ -18,6 +17,19 @@ RSpec.feature 'Listando Treinos', type: feature do
     expect(page).to have_content(@routine2.description)
     expect(page).to have_link(@routine.name)
     expect(page).to have_link(@routine2.name)
+    expect(page).not_to have_link("Novo Treino")
+  end
+
+  scenario 'Um usuário lista todos os treinos e o usuário esta logado' do
+    login_as(@john)
+    visit '/'
+    expect(page).to have_content(@routine.name)
+    expect(page).to have_content(@routine.description)
+    expect(page).to have_content(@routine2.name)
+    expect(page).to have_content(@routine2.description)
+    expect(page).to have_link(@routine.name)
+    expect(page).to have_link(@routine2.name)
+    expect(page).to have_link("Novo Treino")
   end
 
   scenario 'Um usuário não tem treinos cadastrados' do

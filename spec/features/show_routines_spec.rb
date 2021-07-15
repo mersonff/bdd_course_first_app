639ffa48ba873a74b25b18e53df802516e98f12d
Emerson Freitas <mersonsi@gmail.con>
2021-07-15 15:37:21 -0300

    Escondendo links de usuário não logados e usuários não donos dos treinos

--- spec/features/show_routines_spec.rb
+++ spec/features/show_routines_spec.rb
@@ -3,10 +3,29 @@ require 'rails_helper'
 RSpec.feature 'Exibindo Treinos', type: feature do
 
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @jane = User.create(email: "jane@example.com", password: "password")
 
-    @routine = Routine.create(name: 'Treino A', description: 'Supino 3x10', user: john)
+
+    @routine = Routine.create(name: 'Treino A', description: 'Supino 3x10', user: @john)
+  end
+
+  scenario "um usuário não logado não pode ver os links de Editar e Deletar" do
+    visit "/"
+    click_link @routine.name
+    expect(page).to have_content(@routine.name)
+    expect(page).to have_content(@routine.description)
+    expect(current_path).to eq(routine_path(@routine))
+    expect(page).not_to have_link("Editar")
+    expect(page).not_to have_link("Deletar")
+  end
+
+  scenario "um usuário logado mas que não criou os treinos não pode ver os links" do
+    login_as(@jane)
+    visit "/"
+    click_link @routine.name
+    expect(page).not_to have_link("Editar")
+    expect(page).not_to have_link("Deletar")
   end
 
   scenario 'Um usuário exibe um treino' do
